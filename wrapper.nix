9e7ecc753c332f12248f9601c23c3c6192fc3ff1
Alexander Bantyev <balsoft@yandex.ru>
2019-05-27 13:04:13 +0300

    Try to fix makeWrapper

--- wrapper.nix
+++ wrapper.nix
@@ -1,6 +1,7 @@
-{ lib, lambda-launcher-unwrapped, libqalculate, wmctrl
+{ lib, makeWrapper, lambda-launcher-unwrapped, libqalculate, wmctrl
 , plugins ? [ libqalculate wmctrl ] }:
-lambda-launcher-unwrapped.overrideAttrs (_: {
+lambda-launcher-unwrapped.overrideAttrs (old: {
+  buildInputs = old.buildInputs ++ [makeWrapper];
   postInstall = ''
     makeWrapper $out/bin/lambda-launcher $wrapperfile --prefix PATH : ${
       lib.makeBinPath plugins
