2849d0416e56f4f9eca415cbf212339349c5322b
Alexander Bantyev <balsoft75@gmail.com>
2019-11-07 00:58:10 +0300

    Revamp command & add fuzzy command search

--- src/LambdaLauncher/Plugins/Command.hs
+++ src/LambdaLauncher/Plugins/Command.hs
@@ -8,10 +8,18 @@ import System.Environment (getEnv)
 import System.FilePath.Posix (splitSearchPath)
 import System.Directory (doesDirectoryExist, listDirectory)
 import Text.Fuzzy (simpleFilter)
+import Data.List (intersperse)
 import Control.Monad (filterM, void)
 import Data.Text (Text)
 
 import qualified Data.Text as T
+
+removeSame :: Eq a => [a] -> [a] -> [a]
+removeSame found (x:xs) =
+  if x `elem` found
+  then removeSame found xs
+  else removeSame (x:found) xs
+removeSame found _ = found
 
 command :: Text -> IO [Result]
 command query = do
@@ -19,12 +27,13 @@ command query = do
     existEnvs <- filterM doesDirectoryExist envs
     commandList <- mconcat <$> mapM listDirectory existEnvs
     let s = T.unpack query
-    let command = head $ words s
+    let cmd = head $ words s
+    let args = ' ':(intersperse ' ' $ mconcat (tail $ words s))
     let commands =
-          map T.pack $ (++ tail s) <$> simpleFilter command commandList
-    mapM result $ take 3 commands
+          map T.pack $ fmap (++ args) $ simpleFilter cmd commandList
+    mapM result $ take 3 $ removeSame [] commands
   where
     result :: Text -> IO Result
     result x = pure
-      $ Action (T.append "Run command " $ max x query) 1
-      $ void $ spawnCommand $ T.unpack $ max query x
+      $ Action ("Run command " <>  x) 1
+      $ void $ spawnCommand $ T.unpack x
