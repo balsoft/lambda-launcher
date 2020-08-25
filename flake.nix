f125fa4da93dc91ae6ce4d796cc1ff6e14285f34
Alexander Bantyev <balsoft@balsoft.ru>
2020-08-25 11:05:25 +0300

    Fix nix-build

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A launcher written in Haskell";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/dbcd55844106241cf2d6aa0bc6ccb1613d1a5ed4;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
 
   outputs = { self, nixpkgs }: {
     packages = builtins.mapAttrs (_: pkgs: rec {
@@ -15,5 +15,9 @@
       type = "app";
       program = "${pkg}/bin/lambda-launcher";
     }) self.defaultPackage;
+
+    devShell = builtins.mapAttrs (arch: pkgs: pkgs.mkShell {
+      inputsFrom = [ self.packages.${arch}.lambda-launcher-unwrapped self.packages.${arch}.lambda-launcher ];
+    }) nixpkgs.legacyPackages;
   };
 }
