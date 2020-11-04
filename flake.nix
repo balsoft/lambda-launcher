4b617c617c753ec8f28ec1d9cc8a591a5698b6c0
Alexander Bantyev <balsoft@balsoft.ru>
2020-11-05 01:24:32 +0300

    nixfmt flake.nix

--- flake.nix
+++ flake.nix
@@ -1,23 +1,31 @@
 {
   description = "A launcher written in Haskell";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/dbcd55844106241cf2d6aa0bc6ccb1613d1a5ed4;
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/dbcd55844106241cf2d6aa0bc6ccb1613d1a5ed4";
 
   outputs = { self, nixpkgs }: {
     packages = builtins.mapAttrs (_: pkgs: rec {
-      lambda-launcher-unwrapped = pkgs.haskellPackages.callPackage ./lambda-launcher.nix { };
-      lambda-launcher = pkgs.callPackage ./wrapper.nix { inherit lambda-launcher-unwrapped; };
+      lambda-launcher-unwrapped =
+        pkgs.haskellPackages.callPackage ./lambda-launcher.nix { };
+      lambda-launcher =
+        pkgs.callPackage ./wrapper.nix { inherit lambda-launcher-unwrapped; };
     }) nixpkgs.legacyPackages;
 
-    defaultPackage = builtins.mapAttrs (_: pkgs: pkgs.lambda-launcher) self.packages;
+    defaultPackage =
+      builtins.mapAttrs (_: pkgs: pkgs.lambda-launcher) self.packages;
 
     defaultApp = builtins.mapAttrs (_: pkg: {
       type = "app";
       program = "${pkg}/bin/lambda-launcher";
     }) self.defaultPackage;
 
-    devShell = builtins.mapAttrs (arch: pkgs: pkgs.mkShell {
-      inputsFrom = [ self.packages.${arch}.lambda-launcher-unwrapped self.packages.${arch}.lambda-launcher ];
-    }) nixpkgs.legacyPackages;
+    devShell = builtins.mapAttrs (arch: pkgs:
+      pkgs.mkShell {
+        inputsFrom = [
+          self.packages.${arch}.lambda-launcher-unwrapped.env
+          self.packages.${arch}.lambda-launcher
+        ];
+      }) nixpkgs.legacyPackages;
   };
 }
