5370ca905a014211ee7d71b598d5343cdfbf7eeb
Alexander Bantyev <balsoft@balsoft.ru>
2021-06-23 14:26:24 +0300

    makeWrapper -> wrapProgram

--- wrapper.nix
+++ wrapper.nix
@@ -3,8 +3,7 @@
 lambda-launcher-unwrapped.overrideAttrs (old: {
   buildInputs = old.buildInputs ++ [ makeWrapper ];
   postInstall = ''
-    cp $out/bin/lambda-launcher $out/bin/.lambda-launcher-unwrapped
-    makeWrapper $out/bin/.lambda-launcher-unwrapped $out/bin/lambda-launcher --prefix PATH : ${
+    wrapProgram $out/bin/lambda-launcher --prefix PATH : ${
       lib.makeBinPath plugins
     } --prefix LD_LIBRARY_PATH : ${librsvg}/lib
   '';
